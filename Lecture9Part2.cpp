f31dceaa581b58693853c56626436dcf3120eb5e
vishalhandoo <81742803+vishalhandoo@users.noreply.github.com>
2023-06-19 11:58:10 +0530

    Add files via upload

---
+++ Lecture9Part2.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int sum(int a ,int b);
+
+int main(){
+
+    int a=3,b=5;
+
+    cout<<"Sum is: "<<sum(a,b)<<endl;
+    
+    return 0;
+}
+
+int sum(int a,int b){
+    return a+b;
+}
